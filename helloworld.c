9b98e5bc86277a83d77e4c4d27fe5e04fae47bfa
wenhsianglin <wenhsiang@WEN-HSIANGdeMacBook-Pro.local>
2015-07-14 13:13:45 +0800

    add 1*1 ~3*3

--- helloworld.c
+++ helloworld.c
@@ -5,5 +5,12 @@ int main()
 {
   printf("Hello World\n");
   printf("who are you?\n");
+  int i,j;
+  for(i=1;i<=3;i++) {
+    for(j=1;j<=3;j++) {
+      printf("%d * %d = %d\n",i,j,i*j);
+    }
+    printf("\n");
+  }
   return 0;
 }
